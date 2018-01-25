d2a9491115de62375b24aa3d5ae4df26677e7694
Newton Bii <biinewton382@gmail.com>
2018-01-25 16:46:16 +0300

    Add status page.

--- my_insta/views.py
+++ my_insta/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def timelines(request):
-    return render(request, 'timelines.html')
+    working = 'working'
+    return render(request, 'timelines.html', {'working':working})
