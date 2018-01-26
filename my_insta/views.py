da1fdc37af57736a6b38bfebcb5826e6b34e790f
Newton Bii <biinewton382@gmail.com>
2018-01-26 08:35:18 +0300

    Added user stories page.

--- my_insta/views.py
+++ my_insta/views.py
@@ -2,5 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def timelines(request):
-    working = 'working'
-    return render(request, 'timelines.html', {'working':working})
+    return render(request, 'timelines.html')
