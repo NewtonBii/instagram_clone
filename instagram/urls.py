b26e779e1e011752bd92197a5d2c1cced507a46f
Newton Bii <biinewton382@gmail.com>
2018-01-26 10:30:30 +0300

    Install registration and base.html template for registration.

--- instagram/urls.py
+++ instagram/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', include('my_insta.urls'))
+    url(r'', include('my_insta.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls'))
 ]
