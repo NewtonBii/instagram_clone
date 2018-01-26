c3dc3ccf82648b694adec9d985f748f3a7ddee1f
Newton Bii <biinewton382@gmail.com>
2018-01-26 11:03:39 +0300

    Connect to local psql db.

--- instagram/settings.py
+++ instagram/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'instagram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'instagram',
+        'USER': 'newton',
+        'PASSWORD':'newton',
     }
 }
 
