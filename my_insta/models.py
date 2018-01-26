6fda893b40be2783f2b06b18edd128275a9636d2
Newton Bii <biinewton382@gmail.com>
2018-01-26 16:48:09 +0300

    Add timestamp column.

--- my_insta/models.py
+++ my_insta/models.py
@@ -24,3 +24,4 @@ class Profile(models.Model):
     profile_photo = models.ImageField(upload_to = 'profiles/', null=True)
     user_bio = models.TextField()
     user = models.ForeignKey(User)
+    last_update = models.DateTimeField(auto_now_add=True, null=True)
