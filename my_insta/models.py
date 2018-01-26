fe097ece9f4e847bfe37c7f34bfeae7f079b39f1
Newton Bii <biinewton382@gmail.com>
2018-01-26 12:22:49 +0300

    Create models for image, profile and comments.

--- my_insta/models.py
+++ my_insta/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Comments(models.Model):
+    comment = models.TextField()
+    user = models.ForeignKey(User)
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to = 'photos/', null = True)
+    image_name = models.CharField(max_length=30)
+    image_caption = models.TextField()
+    likes = models.IntegerField()
+    comments = models.ManyToManyField(Comments)
+    user = models.ForeignKey(User)
+
+
+class Profile(models.Model):
+    profile_photo = models.ImageField(upload_to = 'profiles/', null=True)
+    user_bio = models.TextField()
+    user = models.ForeignKey(User)
--- my_insta/views.py
+++ my_insta/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def timelines(request):
     return render(request, 'timelines.html')
+
+@login_required(login_url='/accounts/login/')
 def profile(request):
     return render(request, 'profile.html')
