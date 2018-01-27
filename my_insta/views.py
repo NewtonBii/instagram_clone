04051eb975ef32735aa528124fab1174a8626f10
Newton Bii <biinewton382@gmail.com>
2018-01-27 11:40:41 +0300

    Solve current user error.

--- my_insta/views.py
+++ my_insta/views.py
@@ -4,15 +4,18 @@ from .forms import NewStatusForm
 from .models import Image, Profile
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
 def timelines(request):
+    current_user = request.user
     images = Image.objects.order_by('-date_uploaded')
     profiles = Profile.objects.order_by('-last_update')
-    return render(request, 'timelines.html', {'images':images, 'profiles':profiles})
+    user_profile = Profile.objects.get(id = current_user.id)
+    return render(request, 'timelines.html', {'images':images, 'profiles':profiles, 'user_profile':user_profile})
 
 @login_required(login_url='/accounts/login/')
 def profile(request):
     current_user = request.user
-    profile = Profile.objects.get()
+    profile = Profile.objects.get(id = current_user.id)
     return render(request, 'profile.html', {'profile':profile})
 
 @login_required(login_url='/accounts/login/')
